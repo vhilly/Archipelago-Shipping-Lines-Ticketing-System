2b0bbf5e698da736e7926cbe79ed283518279e8d
codemagnet24 <codemagnet24@gmail.com>
2013-06-03 08:37:49 +0800

    rearrange the seat that cause the error

--- protected/views/seat/map.php
+++ protected/views/seat/map.php
@@ -32,21 +32,21 @@
 
 <center>	
     <?php
-$ids = isset($_GET['id'])? $_GET['id'] : '';
-$type = isset($_GET['class']) ? $_GET['class']: 1;
-$skp = Array('45D','45E','45F','45G','44A','44B','44C');
+	$ids = isset($_GET['id'])? $_GET['id'] : '';
+	$type = isset($_GET['class']) ? $_GET['class']: 1;
+	$skp = Array('45D','45E','45F','45G','44A','44B','44C','29D','29F','29G');
         for($n="C";$n<="G";$n++){
-                                        for($m=10;$m<=17;$m++){
-                                                $ap = "$m$n";
-                                                array_push($skp,$ap);
-                                        }
+        	for($m=10;$m<=17;$m++){
+                	$ap = "$m$n";
+                        array_push($skp,$ap);
+                }
         }
-                                switch($type){
-                                  case 1: $cl = "PREMIUM ECONOMY";$min=10;$max=29;break;
-                                        case 2: $cl = "BUSINESS CLASS";$min=1;$max=9;break;
-                                  case 3: $cl = "SUNDECK";$min=30;$max=45;break;
-                                        default:
-                                }
+        switch($type){
+          case 2: $cl = "PREMIUM ECONOMY";$min=10;$max=29;break;
+          case 1: $cl = "BUSINESS CLASS";$min=1;$max=9;break;
+          case 3: $cl = "SUNDECK";$min=30;$max=45;break;
+          default:
+        }
 
 //                                $apr = Array('2A','2B','6D','3C','1G','30D','33G','36E','45A','45B','45C');
 //                                $pres = Array('5D','5E','5F','5G','7A');
@@ -54,46 +54,32 @@ $skp = Array('45D','45E','45F','45G','44A','44B','44C');
 
 
 
-
-                                $body = "";
-                                 for($a="A";$a<="G";$a++){
-                                        if($a=="D"){$body .= "<tr style=\"height:80px;border-left:1px solid transparent;border-right:1px solid transparent\"></tr>";}
-                                   $body .= "<tr>";
-                                   for($b=$min;$b<=$max;$b++){
-                                           $ts = "$b$a";
-                                           if(in_array($ts,$apr)){
-                                                   $cls = "reserve";
-                                                         $clk = "";
-                                                 }elseif(in_array($ts,$pres)){
-                                                   $cls = "pre_assign";
-                                                         $clk = "";
-                                                 }else{
-                                                   $cls = "";
-                                                   $as = "$b$a";
-                                                   $sId = $id[$as];
-                                                         $clk = "onclick=\"get('$b$a','$ids','$sId')\"";
-                                                 }
-                                                if(!in_array($ts,$skp)){
-                                                 if($b==44 && ($a>="A" OR $a<="G")){
-                                                        switch($a){
-                                                         case "D": $c="A";break;
-                                                         case "E": $c="B";break;
-                                                         case "F": $c="C";break;
-                                                         case "G": $c="D";break;
-                                                        }
-                                                        $as = "$b$c";
-                                                        $sId = $id[$as];
-							$clk = $clk!="" ? "onclick=\"get('$b$c','$ids','$sId')\"" : "";
-                                            $body .= "<td class=\"$cls\" $clk >$b$c</td>";
-                                                 }else{
-                                            $body .= "<td class=\"$cls\" $clk >$b$a</td>";
-                                                 }
-                                                }else{
-                                                 $body .= "<th style=\"\"></th>";
-                                                }
-                                         }
-                                         $body .= "</tr>";
-                                 }
+  $body = "";
+  for($a="A";$a<="G";$a++){
+  	if($a=="D"){$body .= "<tr style=\"height:80px;border-left:1px solid transparent;border-right:1px solid transparent\"></tr>";}
+    $body .= "<tr>";
+    for($b=$min;$b<=$max;$b++){
+    	$ts = "$b$a";
+      if(!in_array($ts,$skp)){
+      	if(in_array($ts,$apr)){
+        	$cls = "reserve";
+          $clk = "";
+        }elseif(in_array($ts,$pres)){
+        	$cls = "pre_assign";
+          $clk = "";
+        }else{
+          $cls = "";
+          $s = "$b$a";
+          $ah = $id[$s];
+          $clk = "title=\"$s\" onclick=\"get('$b$a','$ids','$ah')\"";
+        }
+        $body .= "<td class=\"$cls\" $clk >$b$a</td>";
+      }else{
+        $body .= "<th style=\"\"></th>";
+      }
+   	}
+    $body .= "</tr>";
+ 	}
 
 	function econo(){
 		
@@ -108,7 +94,7 @@ $skp = Array('45D','45E','45F','45G','44A','44B','44C');
 	function get(seat,ids,no){
 		if(ids!=""){	
 			$('#'+ids).val(seat);
-			$('#Ticket'+ids).val(no);
+			$('#SeatTicket'+ids).val(no);
 			$('a[data-dismiss="modal"]').click();
 		}
 	}
