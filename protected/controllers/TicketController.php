fe05ff5b7af7e9dbe82e8347f15496e910cf4b37
root <root@enteng.(none)>
2013-06-10 19:16:20 +0800

    added pdf & printController

--- protected/controllers/TicketController.php
+++ protected/controllers/TicketController.php
@@ -164,6 +164,29 @@
         $this->render('index',array('is_empty'=>1,'model'=>$model));
       }
 
+ # mPDF
+        $mPDF1 = Yii::app()->ePdf->mpdf();
+ 
+        # You can easily override default constructor's params
+        $mPDF1 = Yii::app()->ePdf->mpdf('', 'A5');
+ 
+        # render (full page)
+        $mPDF1->WriteHTML($this->render('index', array(), true));
+ 
+        # Load a stylesheet
+        $stylesheet = file_get_contents(Yii::getPathOfAlias('webroot.css') . '/main.css');
+        $mPDF1->WriteHTML($stylesheet, 1);
+ 
+        # renderPartial (only 'view' of current controller)
+        $mPDF1->WriteHTML($this->renderPartial('index', array(), true));
+ 
+        # Renders image
+        $mPDF1->WriteHTML(CHtml::image(Yii::getPathOfAlias('webroot.css') . '/bg.gif' ));
+ 
+        # Outputs ready PDF
+        $mPDF1->Output();
+
+
     }
     /**
      * Manages all models.
