40eee8d5b9ce57c9242be69ff2243f4dd722c64f
vhilly <vhilly@imperium.ph>
2013-08-29 11:52:45 +0800

    upgrade amount

--- protected/controllers/UpgradesController.php
+++ protected/controllers/UpgradesController.php
@@ -36,7 +36,7 @@ array('allow', // allow authenticated user to perform 'create' and 'update' acti
 ),
 array('allow', // allow admin user to perform 'admin' and 'delete' actions
 'actions'=>array('admin','delete'),
-'users'=>array('admin'),
+'users'=>array('@'),
 ),
 array('deny',  // deny all users
 'users'=>array('*'),
